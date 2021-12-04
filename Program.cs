6f189667b181101e219f9c91cd3e1c2f028f19ab
John Bouakhasith <johnbeedot@gmail.com>
2021-12-04 16:25:16 -0500

    Control Flow (if/else/switch)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = Console.ReadLine();
+
+switch (input)
+{
+    case "Hello":
+        Console.WriteLine("Hello, how are you?");
+        break;
+    case "I'm well, thank you.":
+        Console.WriteLine("Good to hear. I'm also well!");
+        break;
+    default:
+        Console.WriteLine("Yeah, this is too advanced for me.");
+        break;
+}
