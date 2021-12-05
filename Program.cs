e07c7874d9ec965b27cbd6ef71a2ab042466e946
John Bouakhasith <johnbeedot@gmail.com>
2021-12-05 18:28:34 -0500

    Practice 1 Prompt

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿var index = 0;
+﻿var i = 0;
 
-while (index < 10)
-{   
-    Console.WriteLine($"Printed from while loop: {index}");
-    index++;
-}
-
-for(var i = 0; i < 10; i++)
+while (i == 0)
 {
-    Console.WriteLine($"Printed from for loop: {i}");
+    Console.Write("Please enter your age: ");
+    var input = Console.ReadLine();
+    int age = Convert.ToInt32(input);
+    int years = 100 - age;
+
+    if (age <= 17)
+    {
+        Console.WriteLine($"Hi there! You are {age} years old.");
+    }
+    else if (age <= 35)
+    {
+        Console.WriteLine($"How's it going? You are {age} years old.");
+    }
+    else if (age <= 75)
+    {
+        Console.WriteLine($"Hello! You are {age} years old.");
+    }
+    else
+    {
+        Console.WriteLine($"Greetings! You are {age} years old.");
+    }
+    Console.WriteLine($"You have {years} until you are 100 years old!");
+    Console.WriteLine();
 }
