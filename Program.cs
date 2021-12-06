75fbafa86e09705af8f1746b244529f513683ce1
John Bouakhasith <johnbeedot@gmail.com>
2021-12-05 21:22:51 -0500

    Functions and Return Types

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿static int myFunction()
+﻿var numberFromFunction = myFunction();
+
+Console.WriteLine($"This is the number from myFunction: {numberFromFunction}");
+
+static int myFunction()
 {
     var myIndex = 0;
     Console.WriteLine(myIndex += 5);
     return myIndex;
 }
-
-myFunction();
