4ae5d369cb816f2085c485447673939682f5049e
John Bouakhasith <johnbeedot@gmail.com>
2021-12-05 20:21:58 -0500

    Functions and Return Types

--- Program.cs
+++ Program.cs
@@ -1,28 +1,8 @@
-﻿var i = 0;
-
-while (i == 0)
+﻿static int myFunction()
 {
-    Console.Write("Please enter your age: ");
-    var input = Console.ReadLine();
-    int age = Convert.ToInt32(input);
-    int years = 100 - age;
-
-    if (age <= 17)
-    {
-        Console.WriteLine($"Hi there! You are {age} years old.");
-    }
-    else if (age <= 35)
-    {
-        Console.WriteLine($"How's it going? You are {age} years old.");
-    }
-    else if (age <= 75)
-    {
-        Console.WriteLine($"Hello! You are {age} years old.");
-    }
-    else
-    {
-        Console.WriteLine($"Greetings! You are {age} years old.");
-    }
-    Console.WriteLine($"You have {years} years left until you are 100 years old!");
-    Console.WriteLine();
+    var myIndex = 0;
+    Console.WriteLine(myIndex += 5);
+    return myIndex;
 }
+
+myFunction();
